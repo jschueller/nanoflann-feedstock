751b3d597a9d33464a3a7fc6c9cdf35fd436c9ca
Julien Schueller <schueller@phimeca.com>
2026-08-22 18:42:37 +0200

    Update to 1.14

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,7 @@
 cmake ^
 	-G "NMake Makefiles" ^
 	-D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+        -DNANOFLANN_BUILD_EXAMPLES=OFF -DNANOFLANN_BUILD_TESTS=OFF ^
 	%SRC_DIR%
 
 if errorlevel 1 exit 1
